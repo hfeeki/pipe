e055a1a7141f381f6b127b1ca069dc96cb3c129c
Paul Bellamy <paul.a.bellamy@gmail.com>
2012-07-10 22:29:38 +0100

    Added Repeat generators

---
+++ repeat.go
@@ -0,0 +1,32 @@
+// Copyright 2012 Paul Bellamy. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package pipe
+
+// Generate an infinite sequence by repeating the value. Can be bounded by passing x.
+func Repeat(item interface{}, x ...int64) chan interface{} {
+	out := make(chan interface{})
+
+	if len(x) > 0 {
+		go boundedRepeatHandler(item, x[0], out)
+	} else {
+		go unboundedRepeatHandler(item, out)
+	}
+
+	return out
+}
+
+func boundedRepeatHandler(item interface{}, bound int64, out chan interface{}) {
+	for i := int64(0); i < bound; i++ {
+		out <- item
+	}
+	close(out)
+}
+
+func unboundedRepeatHandler(item interface{}, out chan interface{}) {
+	for {
+		out <- item
+	}
+	close(out)
+}
